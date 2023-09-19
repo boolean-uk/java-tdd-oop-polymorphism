f64aac9eb8db9343432d85d32ceffb51f989abca
iliask796 <iliaskal7@gmail.com>
2023-09-19 11:19:43 +0300

    Example

---
+++ src/main/java/com/booleanuk/core/vehicles/Mover.java
@@ -0,0 +1,7 @@
+package com.booleanuk.core.vehicles;
+
+public class Mover {
+    public void move(Vehicle vehicle, int metres){
+        vehicle.move(metres);
+    }
+}
---
+++ src/main/java/com/booleanuk/core/vehicles/SkateBoard.java
@@ -0,0 +1,7 @@
+package com.booleanuk.core.vehicles;
+
+public class SkateBoard implements Vehicle{
+    public void move(int metres){
+        System.out.println("You skated " + metres + "m");
+    }
+}
