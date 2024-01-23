c93ff9522807aad55d44717b38be1099402e63e2
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-23 09:10:47 +0100

    Add polymorphism

---
+++ src/main/java/com/booleanuk/core/Item.java
@@ -0,0 +1,17 @@
+package com.booleanuk.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Item {
+
+
+    String name = "";
+    int price = 0;
+
+
+
+    int getPrice();
+
+    String getName();
+}
